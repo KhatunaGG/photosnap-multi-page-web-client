ad1c813115f577fb2ea90c248aaed385845ba58d
Gagloshvili <KGagloshvili@oriflame.com>
2025-06-30 14:28:56 +0400

    added: slider component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,54 @@
 // /** @type {import('tailwindcss').Config} */
+// const plugin = require("tailwindcss/plugin");
+
 // module.exports = {
 //   content: [
-//     './src/app/**/*.{js,ts,jsx,tsx}',
-//     './src/pages/**/*.{js,ts,jsx,tsx}',
-//     './src/components/**/*.{js,ts,jsx,tsx}',
+//     "./src/app/**/*.{js,ts,jsx,tsx}",
+//     "./src/pages/**/*.{js,ts,jsx,tsx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx}",
 //   ],
 //   theme: {
-//     extend: {},
+//     extend: {
+//       fontFamily: {
+//         sans: ['"DM Sans"', "sans-serif"],
+//       },
+//     },
 //   },
-//   plugins: [],
-// }
+//   plugins: [
+//     plugin(function ({ addComponents }) {
+//       addComponents({
+//         ".heading-h2": {
+//           fontSize: "24px",
+//           lineHeight: "25px",
+//           fontWeight: "700",
+//           fontFamily: '"DM Sans", sans-serif',
+//         },
+//         ".heading-h3": {
+//           fontSize: "18px",
+//           lineHeight: "25px",
+//           fontWeight: "700",
+//           fontFamily: '"DM Sans", sans-serif',
+//         },
+//         ".heading-h4": {
+//           fontSize: "12px",
+//           lineHeight: "16px",
+//           fontWeight: "700",
+//           fontFamily: '"DM Sans", sans-serif',
+//         },
+//         ".text-p": {
+//           fontSize: "12px",
+//           lineHeight: "16px",
+//           fontWeight: "700",
+//           fontFamily: '"DM Sans", sans-serif',
+//         },
+//       });
+//     }),
+//   ],
+// };
 
 /** @type {import('tailwindcss').Config} */
 const plugin = require("tailwindcss/plugin");
+const { transform } = require("typescript");
 
 module.exports = {
   content: [
@@ -22,6 +58,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: { "loop-scroll": "loop-scroll 50s linear infinite" },
+      keyframes: {
+        "loop-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" }, 
+        },
+      },
       fontFamily: {
         sans: ['"DM Sans"', "sans-serif"],
       },
