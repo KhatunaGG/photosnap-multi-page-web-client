bd352060be903b7bb3ba985987acacd9350a95dd
Gagloshvili <KGagloshvili@oriflame.com>
2025-06-30 15:37:48 +0400

    added: info component, data

--- src/app/components/__atoms/index.ts
+++ src/app/components/__atoms/index.ts
@@ -1,2 +1,5 @@
 export { default as Burger } from "./burger/Burger";
 export { default as ArrowRight } from "./arrowRight/ArrowRight";
+export { default as Bullhorn } from "./bullhorn/Bullhorn";
+export { default as Forever } from "./forever/Forever"
+export { default as Responsive } from "./responsive/Responsive"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { Dashboard } from "./components/__organism";
 
 export default function Home() {
   return (
-    <section className="w-full min-h-screen bg-green-300  min-h-[calc(100vh-72px)] md:min-h-[calc(100vh-74px)]">
+    <main className="w-full   min-h-[calc(100vh-72px)] md:min-h-[calc(100vh-74px)]">
       <Dashboard />
-    </section>
+    </main>
   );
 }
